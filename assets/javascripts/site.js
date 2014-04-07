31fc4c74d073da320b2aa833e8020c36f9eb8b0d
mrmedina87 <mm@theuxdepartment.com>
2014-04-07 11:54:33 -0300

    drop

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -1,3 +1,4 @@
+// header-menu script
 $(document).ready(function(){
 	    $("a.mobile-btn").click(function (e) {
 			e.preventDefault();
@@ -7,6 +8,7 @@ $(document).ready(function(){
 		});
 	});
 
+//filter acordeon script
 $(document).ready(function(){
 		$(".accordions-wrapper h3.link-acc").click(function(){
 			$(this).next(".acc-item-content").slideToggle("slow");
@@ -25,6 +27,8 @@ $(document).ready(function(){
 
 	});
 
+
+// SLIDES SCRIPT
 $(document).ready(function(){		
 		$(".slider-nav li:nth-child(1) a").click(function(){
 			$("div.promo-h3 > h3.promo").addClass("hide");
@@ -49,19 +53,41 @@ $(document).ready(function(){
 
 	});
 
+// ************   CARDS SELECTOR SCRIPT  *******************
+
+// se cierra al click en el boton de "aplicar"
 $(document).ready(function(){		
 		$("#cards-selection-web .app").click(function(){
-			$("#cards-selection-web.open").toggleClass("open");
+			$("#cards-selection-web.open").toggleClass("open"); 
 		});
 
 	});
 
-$(document).ready(function(){
+//se despliega o pliega el dropdown, version mobile.
+$(document).ready(function(){		
+		$("#cards-selection-mobile").hide();
+		$(".hide-for-medium-up .cards-select-link.no-arrow, #cards-selection-mobile > a.app").click(function(){
+			
+			$("#cards-selection-mobile").slideToggle("slow");
+
+		});
+	});
+
+//para que el evento no se propaga al hacer click en cualquiera de esos 3..
+$(document).ready(function(){		
+		$("header .header-form .search-icon, header .header-form .dir-icon,#cards-selection-mobile > a.app").click(function(e){
+			e.stopPropagation();
+		});
+	});
+
+$(document).ready(function(){ // al Iniciar el Documento..
+
+	//Si la Aerolineas argetinas tiene la On-Check, es la unica que la tendra y se borra en el resto.
 	if( $(".aa-card").hasClass("on-check") ){
 		$(".on-check").removeClass("on-check");
 		$(".aa-card").addClass("on-check");
 	}
-
+	//Sino, se revisa en el resto. Si la cantidad de elementos BUY que tiene on-check es mayor a 1, se deja solo el ultimo
 	else{
 		if ( $(".buy-card.on-check").size() > 1 ) {
 			$(".buy-card.on-check")[$(".buy-card.on-check").size() - 1].className ="buy-card on-check aux";
@@ -70,6 +96,7 @@ $(document).ready(function(){
 			$(".buy-card.aux").removeClass("aux");
 		}// solo el ultimo se queda con la clase on-check
 
+		//Si la cantidad de elementos CREDIT que tiene on-check es mayor a 1, se deja solo el ultimo
 		if ( $(".credit-card.on-check").size() > 1 ){
 			$(".credit-card.on-check")[$(".credit-card.on-check").size() - 1].className ="credit-card on-check aux";
 			$(".credit-card.on-check").removeClass("on-check");
@@ -79,6 +106,9 @@ $(document).ready(function(){
 
 	}
 
+	// IMAGENES DE TARJETAS. Se revisa una por una las imagenes de arriba al dropdown, selector.
+	// Si su correspondiente tiene on-check, se le borra el hidden-card para que pueda verse (inicialmente todas tendrian su 
+	//clase hidden card)
 	if( $(".buy-card:nth-child(1)").hasClass("on-check") ){
 		$(".buy-cards-list > li:nth-child(1)").removeClass("hidden-card");
 	}
@@ -111,13 +141,14 @@ $(document).ready(function(){
 		$(".credit-cards-list > li:nth-child(4)").removeClass("hidden-card");
 	}
 	
-	$(".on-check .check-field").prop('checked',true);
+	$(".on-check .check-field").prop('checked',true); // por ultimo. a todos los que tienen on-check, le tildamos el radiobutt
 
 });
 
-// gral script for radio buttons on the header's dropdown 
+// Script para que se tilde el radiobutt al click
 
 $(document).ready(function(){		
+		//al click en cualquier en li dentro de caso web y mobile.
 
 		$("#cards-selection-web li,#cards-selection-mobile li").click(function(){
 
@@ -134,11 +165,13 @@ $(document).ready(function(){
 				}
 		});
 
+		// al click en un li (web o mobile) que sea BUY
 		$("#cards-selection-web li.buy-card,#cards-selection-mobile li.buy-card").click(function(){
 			$("li.aa-card.on-check").find(".check-field").prop('checked',false);
 			$("li.aa-card.on-check").removeClass("on-check");
 		});
 			
+		// al click en un radio-butt (web o mobile)
 		$("#cards-selection-web li .check-field,#cards-selection-mobile li .check-field").click(function(e){
 			e.stopPropagation();
 
@@ -155,6 +188,7 @@ $(document).ready(function(){
 
 		});
 
+		//igual anterior pero solo BUY
 		$("#cards-selection-web li.buy-card .check-field,#cards-selection-mobile li.buy-card .check-field").click(function(e){
 			e.stopPropagation();
 
@@ -189,7 +223,7 @@ $(document).ready(function(){
 		});
 	});
 
-// script para las paginas que se reemplazan arriba al click en la desplegable - caso web
+// script para las tarjetas que se reemplazan arriba al click en la desplegable - caso web
 $(document).ready(function(){
 	// ************* BUY CARDS ******************
 	$("#cards-selection-web li.buy-card:nth-child(1)").click(function(){
@@ -304,21 +338,11 @@ $(document).ready(function(){
 });
 
 
-$(document).ready(function(){		
-		$("#cards-selection-mobile").hide();
-		$(".hide-for-medium-up .cards-select-link.no-arrow, #cards-selection-mobile > a.app").click(function(){
-			
-			$("#cards-selection-mobile").slideToggle("slow");
 
-		});
-	});
+// *************   END CARD SELECTORS SCRIPT   *******************
 
-$(document).ready(function(){		
-		$("header .header-form .search-icon, header .header-form .dir-icon,#cards-selection-mobile > a.app").click(function(e){
-			e.stopPropagation();
-		});
-	});
 
+// ESTO HACE QUE EL FILTRO DEL SIDEBAR SE ABRA O CIERRE DE ACUERDO A LA RESOLUCION DEL DISPOSITIVO/ VENTANA SIZE.
 $(document).ready(function(){
 		if( $(window).width() > 767){
 				$(".checks-section > h3").addClass("active");
@@ -337,21 +361,55 @@ $(document).ready(function(){
 			}
 			else{
 				$(".checks-section > h3").removeClass("active");
-				$(".checks-section > h3 + ul").slideUp("slow");	
+				$(".checks-section > h3 + ul").slideUp("slow");
 			}
 		});
 });
 
+// ESTO HACE QUE EL PLACEHOLDER DE LOS INPUT DE LA BANDA AMARILLA CAMBIEN SU CONTENIDO DEPENDIENDO DE LA RESOLUCION
 $(document).ready(function(){
 		$(window).resize(function(){
 			if( $(window).width() < 768){
+				// $("#cards-selection-web").hide();
+
 				$(".search-field").attr("placeholder","Ej: Marcas, etc...");
 				$(".dir-field").attr("placeholder","Ej: Direccion, etc...");
 			}
 
 			else{
+				
+
 				$(".search-field").attr("placeholder","Ej: Marcas, Restaurantes, etc...");
 				$(".dir-field").attr("placeholder","Ej: Direccion, Ciudad, etc...");
 			}
 		});
+});
+
+// ESTO HACE QUE SE APLIQUE UN MARGEN A LA BANDA AMARILLA CUANDO SE DESPLGIEGA EL dropdown
+$(document).ready(function(){
+	$(".cards-select-link.no-arrow").click(function(event){
+		
+		if( $("#cards-selection-web").hasClass("open") )
+		    {$("header .band-wrap").removeClass("drop-opened");}
+		else{
+		    $("header .band-wrap").addClass("drop-opened");
+		}
+
+	});
+
+	$("header .main-header").click(function(){
+		if( $("#cards-selection-web").hasClass("open") )
+		    {$("header .band-wrap").removeClass("drop-opened");}
+		else{
+		    // $("header .band-wrap").addClass("drop-opened");
+		}
+	});
+
+	$("header .main-header #cards-selection-web").click(function(e){
+		e.stopPropagation();
+	});
+
+	$("header .main-header #cards-selection-web a.app").click(function(e){
+		$("header .band-wrap").removeClass("drop-opened");		
+	});
 });
